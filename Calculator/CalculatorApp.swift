9b7d1ab215711231b631e8a7ddbf404eb9ff985b
srinivasrowdur <99046278+srinivasrowdur@users.noreply.github.com>
2022-07-31 03:04:06 +0100

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Srinivas Rowdur on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
